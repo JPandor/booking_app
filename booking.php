e51ba78bdf48371f5722b2ee4f166906adf82624
JPandor <88104928+JPandor@users.noreply.github.com>
2022-03-31 22:51:52 +0200

    added methods
    added methods to contribute to oop

--- booking.php
+++ booking.php
@@ -9,6 +9,7 @@ class User {
     public $check_in;
     public $check_out;
     public $amount_days;
+    public $hotel_arr = [];
 
     public function __construct($user_hotel, $first_name, $last_name, $email, $check_in, $check_out, $amount_days){
         $this->user_hotel = $user_hotel;
@@ -20,37 +21,44 @@ class User {
         $this->amount_days = $amount_days;
     }
 
-    public function getHotelDeets($search, $array){
-        $key =  array_search($search, array_column($array, 'name'));
-        $price = $array[$key]['rate'] * $this->amount_days;
-        $features = $array[$key]['features'];
-        $image = $array[$key]['image'];
-        $name = $array[$key]['name'];
+    public function getHotelArray(){
+        //getting json data
+        $json_data = file_get_contents('data.json');
+        $json_data = json_decode($json_data);
+        //converting json objects into an array
+        foreach ($json_data as $value){
+            $hotel = (array) $value;
+            array_push($this->hotel_arr, $hotel);
+        }
+    }
+
+    public function getHotelDeets($search){
+        $key =  array_search($search, array_column($this->hotel_arr, 'name'));
+        $price = $this->hotel_arr[$key]['rate'] * $this->amount_days;
+        $features = $this->hotel_arr[$key]['features'];
+        $image = $this->hotel_arr[$key]['image'];
+        $name = $this->hotel_arr[$key]['name'];
         echo "<h1>Hello " .  $this->first_name . $this->last_name . "</h1>";
         echo "These are your entered details:" . "<br>";
         echo "Email: " . $this->email . "<br> Hotel: " . $this->user_hotel . "<br> Check-in: " . $this->check_in . "<br> Check-out: " . $this->check_out . "<br> Amount of days: " . $this->amount_days ."<br>"; 
         echo $this->user_hotel . " has the following features: " . $features;
         echo "<h3>Your price for " . $this->amount_days . " night/s is R" . $price . ".00</h3>";
     }
+
+    
     
 }
 
-//getting json data
-$json_data = file_get_contents('data.json');
-$json_data = json_decode($json_data);
 
-//converting json objects into an array
-$hotel_arr = [];
-foreach ($json_data as $value){
-    $hotel = (array) $value;
-    array_push($hotel_arr, $hotel);
-}
 
 //instantiate user class
 $user1 = new User ($_POST['hotel'], $_POST['first_name'], $_POST['last_name'], $_POST['email'], $_POST['check_in'], $_POST['check_out'], $_POST['amount_days']);
 
 //run method from class
-$user1->getHotelDeets($user1->user_hotel, $hotel_arr);
+$user1->getHotelArray();
+$user1->getHotelDeets($user1->user_hotel);
+
+$user1->getClosest();
 
 
 ?>
--- index.php
+++ index.php
@@ -12,58 +12,59 @@ $hotels = json_decode($hotels);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://unpkg.com/vue@3"></script>
-    <link href='styles.css'>
+    <link href='styles.css' rel='stylesheet'>
     <title>Book Your Hotel</title>
 </head>
 <body>
-    <h1>Book hotels at the cheapest prices</h1>
-    <form action='booking.php' method='post'>
-        <label for='first_name'>First Name</label>
-        <input type='text' id='first_name' name='first_name' required>
-
-        <label for='last_name'>Last Name</label>
-        <input type='text' id='last_name' name='last_name' required>
-
-        <label for='email'>Email</label>
-        <input type='email' id='email' name='email' required>
-
-        <label for='amount_days'>Number Of Days</label>
-        <input type='number' id='amount_days' name='amount_days' min='1' required>
-
-        <label for='check_in'>Check-In Date</label>
-        <input type='date' id='check_in' name='check_in' required>
-
-        <label for='check-out'>Check-Out Date</label>
-        <input type='date' id='check_out' name='check_out' required>
-
-        <label for='hotel'>Hotel</label>
-        <select id="hotel" name="hotel" required>
-            <option value="Hilton">Hilton</option>
-            <option value="Conrad">Conrad</option>
-            <option value="Grand Orchid">Grand Orchid</option>
-            <option value="Cabana Beach">Cabana Beach</option>
-            <option value="Ngwenya Lodge">Ngwenya Lodge</option>
-            <option value="Kruger Park Lodge">Kruger Park Lodge</option>
-            <option value="Marriott">Marriott</option>
-            <option value="Southern Sun">Southern Sun</option>
-            <option value="Durban Spa">Durban Spa</option>
-            <option value="The Palace">The Palace</option>
-        </select>
-
-        <label for='compare'>Compare</label>
-        <input type='submit' id='compare' name='compare'>
-    </form>
-
-    <div id='app'>
-        <ul>
-            <li v-for='(hotel, index) in hotel_arr' :key='index'>
-                <img v-bind:src='hotel[index].image'>
-                <h4> {{ hotel[index].name }}</h4>
-                <p> {{ hotel[index].rate }} </p>
-                <p> {{ hotel[index].features }} </p>
-            </li>
-        </ul>
-    </div>
+    
+        <h1>Book hotels at the cheapest prices</h1>
+    <main id='container'>
+        <div class='flex'>
+            <form action='booking.php' method='post'>
+                <label for='first_name'>First Name</label>
+                <input type='text' id='first_name' name='first_name' required>
+                <label for='last_name'>Last Name</label>
+                <input type='text' id='last_name' name='last_name' required>
+                <label for='email'>Email</label>
+                <input type='email' id='email' name='email' required>
+                <label for='amount_days'>Number Of Days</label>
+                <input type='number' id='amount_days' name='amount_days' min='1' required>
+                <label for='check_in'>Check-In Date</label>
+                <input type='date' id='check_in' name='check_in' required>
+                <label for='check-out'>Check-Out Date</label>
+                <input type='date' id='check_out' name='check_out' required>
+                <label for='hotel'>Hotel</label>
+                <select id="hotel" name="hotel" required>
+                    <option value="Hilton">Hilton</option>
+                    <option value="Conrad">Conrad</option>
+                    <option value="Grand Orchid">Grand Orchid</option>
+                    <option value="Cabana Beach">Cabana Beach</option>
+                    <option value="Ngwenya Lodge">Ngwenya Lodge</option>
+                    <option value="Kruger Park Lodge">Kruger Park Lodge</option>
+                    <option value="Marriott">Marriott</option>
+                    <option value="Southern Sun">Southern Sun</option>
+                    <option value="Durban Spa">Durban Spa</option>
+                    <option value="The Palace">The Palace</option>
+                </select>
+                <label for='compare'>Compare</label>
+                <input type='submit' id='compare' name='compare'>
+            </form>
+        </div>
+        <div id='app' class='flex'>
+            <ul>
+                <li v-for='(hotel, index) in hotel_arr' :key='index'>
+                    <ul>
+                        <li v-for='item in hotel' class='media-box'>
+                            <img v-bind:src='item.image'>
+                            <h4> {{ item.name }}</h4>
+                            <p> {{ item.rate }} </p>
+                            <p> {{ item.features }} </p>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </main>
     <script src='script.js'></script>
 </body>
 </html>
